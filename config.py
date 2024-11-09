dafc899a79801bf19854f9f22da508db321426e4
Mete Ismayilzada <mismayilza@gmail.com>
2024-11-09 18:57:29 +0100

    customize start date

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
 USERNAME = "mismayil"
 
@@ -5,7 +7,8 @@ USERNAME = "mismayil"
 REPO_OWNER = "mismayil"
 REPO_NAME = "daily-arxiv-notifications"
 
+from_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
 # Set submission url(s) of subject
-SEARCH_URLS = [f"https://arxiv.org/search/advanced?advanced=&terms-0-operator=AND&terms-0-term=creative&terms-0-field=all&terms-1-operator=OR&terms-1-term=creativity&terms-1-field=all&classification-computer_science=y&classification-physics_archives=all&classification-include_cross_list=include&date-year=&date-filter_by=date_range&date-from_date=2024-11-08&date-to_date=&date-date_type=announced_date_first&abstracts=show&size=200&order=-announced_date_first"]
+SEARCH_URLS = [f"https://arxiv.org/search/advanced?advanced=&terms-0-operator=AND&terms-0-term=creative&terms-0-field=all&terms-1-operator=OR&terms-1-term=creativity&terms-1-field=all&classification-computer_science=y&classification-physics_archives=all&classification-include_cross_list=include&date-year=&date-filter_by=date_range&date-from_date={from_date}&date-to_date=&date-date_type=announced_date_first&abstracts=show&size=200&order=-announced_date_first"]
 
 KEYWORDS = ["creativity", "creative"]
