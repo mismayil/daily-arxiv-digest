3cc5fbf0fc7b98b18efc8686f6a208109b763ae5
Mete Ismayilzada <mismayilza@gmail.com>
2024-11-17 15:43:51 +0100

    add support to skip already notified papers

--- main.py
+++ main.py
@@ -12,9 +12,20 @@ def get_paper_id(result):
     match = re.search(r'arXiv:\d+\.\d+(v\d+)?', paper_number)
     if match:
         arxiv_id_text = match.group(0)
-        return arxiv_id_text.split(":")[-1]
+        return arxiv_id_text.split(":")[-1].strip()
+
+def load_past_papers():
+    with open("past_papers.txt", "r") as file:
+        content = file.read()
+        return content.split(";")
+
+def add_to_past_papers(papers):
+    with open("past_papers.txt", "a") as file:
+        file.write(";")
+        file.write(";".join(papers))
 
 def main():
+    past_papers = load_past_papers()
     for search_url in SEARCH_URLS:
         page = urllib.request.urlopen(search_url)
         soup = bs(page, "html.parser")
@@ -28,6 +39,11 @@ def main():
         for result in result_list:
             paper = {}
             paper_number = get_paper_id(result)
+
+            if paper_number in past_papers:
+                continue
+            
+            paper["number"] = paper_number
             paper["url"] = arxiv_base + paper_number
             paper["pdf"] = arxiv_base.replace("abs", "pdf") + paper_number
             paper["title"] = result.find("p", {"class": "title is-5 mathjax"}).text.strip()
@@ -45,6 +61,7 @@ def main():
             full_report = full_report + report + "\n\n"
 
         make_github_issue(title=issue_title, body=full_report, labels=KEYWORDS)
+        add_to_past_papers([paper["number"] for paper in papers])
 
 if __name__ == "__main__":
     main()
