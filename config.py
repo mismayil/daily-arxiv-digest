8b84d87e2e90261725bc035ff7f6517794422f69
Mete Ismayilzada <mismayilza@gmail.com>
2025-03-25 11:02:24 +0100

    fix monday reporting issue

--- config.py
+++ config.py
@@ -7,7 +7,13 @@ USERNAME = "mismayil"
 REPO_OWNER = "mismayil"
 REPO_NAME = "daily-arxiv-digest"
 
-from_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+num_days = 1
+
+# if it is monday, get the papers from friday, saturday and sunday
+if datetime.today().weekday() == 0:
+    num_days = 3
+
+from_date = (datetime.now() - timedelta(days=num_days)).strftime("%Y-%m-%d")
 # Set submission url(s) of subject
 SEARCH_URLS = [f"https://arxiv.org/search/advanced?advanced=&terms-0-operator=AND&terms-0-term=creative&terms-0-field=all&terms-1-operator=OR&terms-1-term=creativity&terms-1-field=all&classification-computer_science=y&classification-physics_archives=all&classification-include_cross_list=include&date-year=&date-filter_by=date_range&date-from_date={from_date}&date-to_date=&date-date_type=submitted_date&abstracts=show&size=200&order=-announced_date_first"]
 
